9a36787e79f02ea5ee1f001c7aada85881b3abdd
jinmokju <jinmokju@gmail.com>
2026-04-10 17:56:28 +0900

    Configure Supabase public client keys

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
-﻿window.VVR_SUPABASE_URL = window.VVR_SUPABASE_URL || "";
-window.VVR_SUPABASE_ANON_KEY = window.VVR_SUPABASE_ANON_KEY || "";
+﻿window.VVR_SUPABASE_URL = "https://qrxvzyrthgmypgrbnptf.supabase.co";
+window.VVR_SUPABASE_ANON_KEY = "sb_publishable_A_b1gc5FgAZMycMNX1kalw_GvHIsWfP";
